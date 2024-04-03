dc7d1340f885c2b9b4271163e015864722baff4e
Ignasi Perez-Valls <ignasiperez@gmail.com>
2024-04-03 17:51:16 +0200

    e - New Xcode (15.3) project - Interface: SwiftUI, Language: Swift, Include Tests: on

---
+++ Memorize Bridge/Memorize Bridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize Bridge
+//
+//  Created by Ignasi Perez-Valls on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize Bridge/Memorize Bridge/Memorize_BridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_BridgeApp.swift
+//  Memorize Bridge
+//
+//  Created by Ignasi Perez-Valls on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_BridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
